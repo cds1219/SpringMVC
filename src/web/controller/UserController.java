beca690b55ac80548c927a07cde539104e08a365
cds1219 <cds1219_@naver.com>
2021-06-28 14:56:15 +0900

    MultiActionController

---
+++ src/web/controller/UserController.java
@@ -0,0 +1,14 @@
+package web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class UserController extends MultiActionController{
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav=new ModelAndView("result");
+		return mav;
+	}
+}
