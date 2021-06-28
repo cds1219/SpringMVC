1d4da4ac8094bf9e6c9f1b58b8fb50e6ca2c9feb
cds1219 <cds1219_@naver.com>
2021-06-28 15:50:59 +0900

    url-method mapping

---
+++ WebContent/test/memberInfo.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>회원정보 확인</h1>
+	아이디: ${loginedId }
+	<br>패스워드: ${loginedPw }
+</body>
+</html>
--- src/web/controller/UserController.java
+++ src/web/controller/UserController.java
@@ -7,8 +7,31 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class UserController extends MultiActionController{
+	
+	public ModelAndView memberInfo(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		ModelAndView mav=new ModelAndView();
+		
+		String id=request.getParameter("id");
+		String pw=request.getParameter("pw");
+		
+		//DB
+		mav.addObject("loginedId",id);
+		mav.addObject("loginedPw",pw);
+		
+		mav.setViewName("memberInfo");
+		return mav;
+	}
+	
 	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception{
-		ModelAndView mav=new ModelAndView("result");
+		ModelAndView mav=new ModelAndView();
+		
+		String id=request.getParameter("id");
+		String pw=request.getParameter("pw");
+		
+		//DB
+		mav.addObject("loginedId",id);
+		
+		mav.setViewName("result");
 		return mav;
 	}
 }
