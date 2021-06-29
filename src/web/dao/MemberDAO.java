4744fc38695119f172a8759f5e4889f9ce3cbfae
cds1219 <cds1219_@naver.com>
2021-06-29 14:28:21 +0900

    mybatis

--- src/web/dao/MemberDAO.java
+++ src/web/dao/MemberDAO.java
@@ -1,44 +1,39 @@
 package web.dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.core.JdbcTemplate;	//SpringJDBC core API
-import org.springframework.jdbc.core.RowMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import web.vo.MemberVO;
 
-public class MemberDAO {
+public class MemberDAO {	
 	
-	JdbcTemplate jdbcTemplate;
-
-	public void setDataSource(DataSource dataSource) {
-		jdbcTemplate=new JdbcTemplate(dataSource);
-	}
-
-	public List<MemberVO> listMembers() {	// selectAllMembers()와 동일
-		String sql="select * from member";
-		
-		List<MemberVO> list=new ArrayList();
-		list=jdbcTemplate.query(sql, new RowMapper(){	//anonymous local class
-
-			@Override
-			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
-				MemberVO vo=new MemberVO();
-				vo.setId(rs.getString("id"));
-				vo.setPw(rs.getString("pw"));
-				vo.setName(rs.getString("name"));
-				System.out.println(vo);
-				return vo;
+	private static SqlSessionFactory sqlMapper=null;
+	
+	public static SqlSessionFactory getInstance() {
+		if(sqlMapper==null) {
+			try {
+				String resource="mybatis/SqlMapConfig.xml";
+				Reader reader = Resources.getResourceAsReader(resource);
+				sqlMapper=new SqlSessionFactoryBuilder().build(reader);
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			
-		});	//con대여+PreparedStatement+executeQuery
-		
-		return list;
+		}
+		return sqlMapper;
 	}
 	
+	public List<MemberVO> listMembers() {
+		sqlMapper=getInstance();
+		SqlSession session=sqlMapper.openSession();	//DB connection
+		List<MemberVO> list=session.selectList("mapper.member.selectAllMemberList");
+		return list;
+
+	}	
 }
---
+++ src/web/dao/MemberDAO_SpringJDBC.java
@@ -0,0 +1,44 @@
+package web.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;	//SpringJDBC core API
+import org.springframework.jdbc.core.RowMapper;
+
+import web.vo.MemberVO;
+
+public class MemberDAO_SpringJDBC {
+	
+	JdbcTemplate jdbcTemplate;
+
+	public void setDataSource(DataSource dataSource) {
+		jdbcTemplate=new JdbcTemplate(dataSource);
+	}
+
+	public List<MemberVO> listMembers() {	// selectAllMembers()와 동일
+		String sql="select * from member";
+		
+		List<MemberVO> list=new ArrayList();
+		list=jdbcTemplate.query(sql, new RowMapper(){	//anonymous local class
+
+			@Override
+			public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+				MemberVO vo=new MemberVO();
+				vo.setId(rs.getString("id"));
+				vo.setPw(rs.getString("pw"));
+				vo.setName(rs.getString("name"));
+				System.out.println(vo);
+				return vo;
+			}
+			
+		});	//con대여+PreparedStatement+executeQuery
+		
+		return list;
+	}
+	
+}
